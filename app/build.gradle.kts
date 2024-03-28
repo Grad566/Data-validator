3981aa06d8782be805ae03d9472339fe8adb0a22
Denis Orekhov <3008denis@gmail.com>
2024-03-28 11:19:06 +0500

    Create and set project

---
+++ app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("java")
+    checkstyle
+    application
+    jacoco
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass = "hexlet.code.App";
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required = true
+    }
+}
