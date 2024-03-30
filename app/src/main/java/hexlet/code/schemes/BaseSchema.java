633695fce6cf059a215b7afd75b7b95564b00049
Denis Orekhov <3008denis@gmail.com>
2024-03-30 15:47:00 +0500

    Add new  methoods to NumberSchema

--- app/src/main/java/hexlet/code/schemes/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemes/BaseSchema.java
@@ -1,2 +1,15 @@
-package hexlet.code.schemes;public class BaseSchema {
+package hexlet.code.schemes;
+
+public abstract class BaseSchema<T> {
+    protected boolean restriction;
+
+    public BaseSchema() {
+        restriction = false;
+    }
+
+    public void required() {
+        restriction = true;
+    }
+
+    public abstract boolean isValid(T value);
 }
--- app/src/main/java/hexlet/code/schemes/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemes/NumberSchema.java
@@ -1,2 +1,48 @@
-package hexlet.code.schemes;public class NumberSchema {
+package hexlet.code.schemes;
+
+public class NumberSchema extends BaseSchema<Integer> {
+    private boolean positiveRestriction;
+    private Integer minValue;
+    private Integer maxValue;
+    private boolean rangeRestriction;
+
+    public NumberSchema() {
+        super();
+        positiveRestriction = false;
+        minValue = Integer.MIN_VALUE;
+        maxValue = Integer.MAX_VALUE;
+        rangeRestriction = false;
+    }
+    @Override
+    public boolean isValid(Integer value) {
+        if (value == null && (restriction || positiveRestriction || rangeRestriction)) {
+            return false;
+        } else if (value == null) {
+            return true;
+        }
+
+        return isInRange(value) && isPositive(value);
+    }
+
+    public void positive() {
+        positiveRestriction = true;
+    }
+
+    public void range(int num1, int num2) {
+        minValue = num1;
+        maxValue = num2;
+        rangeRestriction = true;
+    }
+
+    private boolean isPositive(int num) {
+        if (!positiveRestriction) {
+            return true;
+        } else {
+            return num >= 0;
+        }
+    }
+
+    private boolean isInRange(int num) {
+        return num >= minValue && num <= maxValue;
+    }
 }
