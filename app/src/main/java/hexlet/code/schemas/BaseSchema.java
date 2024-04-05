3790f034ec8958f69ecf63a73200dc8dbddcd3e4
Denis Orekhov <3008denis@gmail.com>
2024-04-05 09:55:13 +0500

    Refactoring isValid

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -2,22 +2,27 @@ package hexlet.code.schemas;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 public abstract class BaseSchema<T> {
     protected List<Predicate<T>> validations;
+    protected Predicate<T> notNull = Objects::nonNull;
 
     public BaseSchema() {
         validations = new ArrayList<>();
     }
     // накладывает ограничения на null и частные случаи в зависимости от типа данных
-    public abstract BaseSchema<T> required();
+    public BaseSchema<T> required() {
+        validations.add(notNull);
+        return this;
+    }
 
     // проверяет валидность данных
     public boolean isValid(T value) {
 
-        if (value == null && validations.isEmpty()) {
-            return true;
+        if (value == null && validations.contains(notNull)) {
+            return false;
         } else if (value == null) {
             return true;
         }
